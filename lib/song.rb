d2cf64f2f7ed9b856c252f527449d3efce27bd23
Danny Brandt <dvbrandt90@gmail.com>
2021-01-12 20:16:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   extend Memorable::ClassInstances
   extend Findable::ClassInstances
-  
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
